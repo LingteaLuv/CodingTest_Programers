86f5225abec27df658f42f954a340584fd93e166
01stone <hagwhr2@gmail.com>
2025-03-22 23:34:54 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.24 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 1;
+        
+        while(answer * 7 < n)
+        {
+            answer++;
+        }   
+        return answer;
+    }
+}
