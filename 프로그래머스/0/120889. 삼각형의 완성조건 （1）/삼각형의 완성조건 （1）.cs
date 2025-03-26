6b3cb60939ce5ed1391eebba814a77fd81dc92c6
01stone <hagwhr2@gmail.com>
2025-03-26 21:03:38 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.28 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int[] sides) 
+    {
+        int answer = 2;
+        if((sides[0]+sides[1]>sides[2]) && (sides[0]+sides[2]>sides[1]) && (sides[1]+sides[2]>sides[0]))
+        {
+            answer = 1;
+        }
+        return answer;
+    }
+}
