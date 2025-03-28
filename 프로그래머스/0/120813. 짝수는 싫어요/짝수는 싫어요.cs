e76774cd41e79065ba2e12a17cd7aa72f267262f
01stone <hagwhr2@gmail.com>
2025-03-28 23:21:31 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.20 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.cs
@@ -0,0 +1,27 @@
+using System;
+
+public class Solution {
+    public int[] solution(int n) {
+        ;
+        if(n % 2 == 0)
+        {    int[] answer = new int[ n / 2];
+        
+            for(int i = 0; i < (n / 2); i++)
+        {
+                answer[i] = i * 2 + 1;
+        }
+         return answer;
+        }
+        else
+        {
+            
+            int[] answer = new int[ n / 2 + 1];
+        for(int i = 0; i < (n / 2) + 1; i++)
+        {
+                answer[i] = i * 2 + 1;
+        }
+            return answer;
+        }
+        
+    }
+}
