e12c822617031f42b154ec03692dbfe550dcfb0a
lingtea_luv <hagwhr2@gmail.com>
2025-05-11 19:07:18 +0900

    [level 0] Title: 로그인 성공?, Time: 0.25 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.cs
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.cs
@@ -3,7 +3,7 @@ using System;
 public class Solution {
     public string solution(string[] id_pw, string[,] db) 
     {
-        {
+        
             for (int i = 0; i < db.GetLength(0); i++)
             {
                 if (db[i, 0] == id_pw[0])
@@ -13,6 +13,6 @@ public class Solution {
                 }
             }
             return "fail";
-        }
+        
     }
 }
